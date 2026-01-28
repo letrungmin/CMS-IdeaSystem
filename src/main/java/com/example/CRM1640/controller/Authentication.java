36507aa5c8aceb081f0ba8d2d942b483562d3a39
thanh090902 <thanhthaiit92@gmail.com>
2026-01-28 17:29:27 +0700

    First Commit

---
+++ src/main/java/com/example/CRM1640/controller/Authentication.java
@@ -0,0 +1,9 @@
+package com.example.CRM1640.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class Authentication {
+}
