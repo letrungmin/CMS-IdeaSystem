748c522607156723f9e37e491b8a3f5f6155e80e
thanh090902 <thanhthaiit92@gmail.com>
2026-01-28 17:36:09 +0700

    First config base

---
+++ src/main/java/com/example/CRM1640/Crm1640Application.java
@@ -0,0 +1,13 @@
+package com.example.CRM1640;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crm1640Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crm1640Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CRM1640/Crm1640ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRM1640;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Crm1640ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
