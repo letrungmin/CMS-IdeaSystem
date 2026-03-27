04b2cf196b50b23b6564ebfc30c60ea58d8519e6
thanh090902 <thanhthaiit92@gmail.com>
2026-03-27 19:59:31 +0700

    feat(backend): Config cors for Frontend

--- src/main/java/com/example/CRM1640/config/SecurityConfig.java
+++ src/main/java/com/example/CRM1640/config/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.*;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -13,5 +17,21 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         // Argon2 – recommended
         return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 }
